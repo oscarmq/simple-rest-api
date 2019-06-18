67bc33b16ee0157de212668942df61f56972a6dd
Oscar Machado Quintela <oscarmachadoquintela@gmail.com>
2019-06-18 23:04:25 +0200

    Add persistence to Todos API

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,76 @@
 import express from 'express'
 import Joi from 'joi'
+import mongoose from 'mongoose'
+import { DB_URL, DB_USER, DB_USER_PASSWORD } from './dbcredentials'
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_URL}`, { useNewUrlParser: true })
+
+const todoSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+
 const app = express()
 
 app.use(express.json())
 
-interface ITodo {
-    id: number,
-    description: string
-}
+app.get('/api/todos', async (req, res) => {
+    const todos = await Todo.find().sort('description')
 
-const todos = [
-    { id: 1, description: 'ride your bike' },
-    { id: 2, description: 'ride again' },
-    { id: 3, description: 'learn something interesting' }
-]
+    res.send(todos)
+})
 
-const validateTodo = (todo: ITodo) => {
+app.get('/api/todos/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo) return res.status(404).send('There is no todo with the given ID')
+
+    res.send(todo)
+})
+
+app.post('/api/todos', async (req, res) => {
+    const { error } = validateTodo(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    let todo = new Todo({ description: req.body.name })
+    todo = await todo.save()
+
+    res.send(todo)
+})
+
+app.put('/api/todos/:id', async (req, res) => {
+    const { error } = validateTodo(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const todo = await Todo.findByIdAndUpdate(req.params.id, { description: req.body.description }, {
+        new: true
+    })
+
+    if (!todo) return res.status(404).send('There is no todo with the given ID')
+
+    res.send(todo)
+})
+
+app.delete('/api/todos/:id', async (req, res) => {
+    const todo = await Todo.findByIdAndRemove(req.params.id)
+
+    if (!todo) return res.status(404).send('There is no todo with the given ID')
+
+    res.send(todo)
+})
+
+const validateTodo = (todo: any) => {
     const schema = {
         description: Joi.string().required()
     }
     return Joi.validate(todo, schema)
 }
 
-app.get('/api/todos', (req, res) => res.send(todos))
-
-app.get('/api/todos/:id', (req, res) => {
-    const todo = todos.find((t) => t.id === parseInt(req.params.id))
-
-    if (!todo) {
-        return res.status(404).send('There is no todo with the given ID')
-    }
-    res.send(todo)
-})
-
-app.post('/api/todos', (req, res) => {
-    const { error } = validateTodo(req.body)
-    if (error) {
-        return res.status(400).send(error.details[0].message)
-    }
-
-    const todo = {
-        description: req.body.description,
-        id: todos.length + 1
-    }
-
-    todos.push(todo)
-    res.send(todo)
-})
-
-app.put('/api/todos/:id', (req, res) => {
-    const todo = todos.find((t) => t.id === parseInt(req.params.id))
-
-    if (!todo) {
-        return res.status(404).send('There is no todo with the given ID')
-    }
-
-    const { error } = validateTodo(req.body)
-    if (error) {
-        return res.status(400).send(error.details[0].message)
-    }
-
-    todo.description = req.body.description
-    res.send(todo)
-})
-
-app.delete('/api/todos/:id', (req, res) => {
-    const todo = todos.find((t) => t.id === parseInt(req.params.id))
-
-    if (!todo) {
-        return res.status(404).send('There is no todo with the given ID')
-    }
-
-    const index = todos.indexOf(todo)
-    todos.splice(index, 1)
-
-    res.send(todo)
-})
-
 const port = process.env.PORT || 3000
-// tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`))
